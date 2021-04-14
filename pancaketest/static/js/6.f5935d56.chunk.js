44cf5dcaae3c67786e129f54b5b2a09b83017fde
peterch861 <peterch861@gmail.com>
2021-04-14 11:20:10 +0700

    add pancaketest

---
+++ pancaketest/static/js/6.f5935d56.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@pancakeswap/interface"]=this["webpackJsonp@pancakeswap/interface"]||[]).push([[6],{705:function(n,c){},706:function(n,c){},715:function(n,c){},716:function(n,c){},736:function(n,c){},739:function(n,c){},740:function(n,c){},745:function(n,c){},749:function(n,c){},802:function(n,c){}}]);
+//# sourceMappingURL=6.f5935d56.chunk.js.map
---
+++ pancaketest/static/js/7.ecb4cafe.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@pancakeswap/interface"]=this["webpackJsonp@pancakeswap/interface"]||[]).push([[7],{1065:function(n,c){},705:function(n,c){},706:function(n,c){},715:function(n,c){},716:function(n,c){},736:function(n,c){},739:function(n,c){},740:function(n,c){},745:function(n,c){},749:function(n,c){}}]);
+//# sourceMappingURL=7.ecb4cafe.chunk.js.map
